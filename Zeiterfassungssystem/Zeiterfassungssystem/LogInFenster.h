0bd47869502f34b5d25d159cbc966c667523daa0
Jannick Schwartz <jannick.schwartz@gmail.com>
2018-08-27 18:47:02 +0200

    Fehler Commit

--- Zeiterfassungssystem/Zeiterfassungssystem/LogInFenster.h
+++ Zeiterfassungssystem/Zeiterfassungssystem/LogInFenster.h
@@ -8,32 +8,46 @@ namespace Zeiterfassungssystem {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Collections::Generic;
+	using namespace System::Media;
+	//Namespace zum lesen und schreiben
+	using namespace System::Runtime::Serialization::Formatters::Binary;
+	using namespace System::IO;
 
 	/// <summary>
-	/// Zusammenfassung für LogInFenster
+	/// Zusammenfassung für loginFenster
 	/// </summary>
-	public ref class LogInFenster : public System::Windows::Forms::Form
+	public ref class loginFenster : public System::Windows::Forms::Form
 	{
+	private:
+		SoundPlayer^ sound;
+
 	public:
-		LogInFenster(void)
+		loginFenster(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Konstruktorcode hier hinzufügen.
-			//
+			sound = gcnew SoundPlayer();
 		}
 
 	protected:
 		/// <summary>
 		/// Verwendete Ressourcen bereinigen.
 		/// </summary>
-		~LogInFenster()
+		~loginFenster()
 		{
 			if (components)
 			{
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  txt_Kennwort;
+	private: System::Windows::Forms::Button^  btn_passwortAendern;
+	private: System::Windows::Forms::Button^  logInButton;
+	private: System::Windows::Forms::Label^  BenutzernameLabel;
+	private: System::Windows::Forms::Label^  KennwortLabel;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::Button^  passwortvergessenButton;
+	private: System::Windows::Forms::TextBox^  txt_Benutzername;
 
 	private:
 		/// <summary>
@@ -48,18 +62,150 @@ namespace Zeiterfassungssystem {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(loginFenster::typeid));
+			this->logInButton = (gcnew System::Windows::Forms::Button());
+			this->BenutzernameLabel = (gcnew System::Windows::Forms::Label());
+			this->KennwortLabel = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->passwortvergessenButton = (gcnew System::Windows::Forms::Button());
+			this->txt_Benutzername = (gcnew System::Windows::Forms::TextBox());
+			this->txt_Kennwort = (gcnew System::Windows::Forms::TextBox());
+			this->btn_passwortAendern = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// LogInFenster
+			// logInButton
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->logInButton->BackColor = System::Drawing::Color::MediumBlue;
+			this->logInButton->Font = (gcnew System::Drawing::Font(L"Arial", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->logInButton->ForeColor = System::Drawing::SystemColors::Control;
+			this->logInButton->Location = System::Drawing::Point(30, 152);
+			this->logInButton->Name = L"logInButton";
+			this->logInButton->Size = System::Drawing::Size(112, 45);
+			this->logInButton->TabIndex = 2;
+			this->logInButton->Text = L"Einloggen";
+			this->logInButton->UseVisualStyleBackColor = false;
+			// 
+			// BenutzernameLabel
+			// 
+			this->BenutzernameLabel->AutoSize = true;
+			this->BenutzernameLabel->Font = (gcnew System::Drawing::Font(L"Arial", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->BenutzernameLabel->Location = System::Drawing::Point(27, 33);
+			this->BenutzernameLabel->Name = L"BenutzernameLabel";
+			this->BenutzernameLabel->Size = System::Drawing::Size(127, 16);
+			this->BenutzernameLabel->TabIndex = 2;
+			this->BenutzernameLabel->Text = L"Personalnummer";
+			// 
+			// KennwortLabel
+			// 
+			this->KennwortLabel->AutoSize = true;
+			this->KennwortLabel->Font = (gcnew System::Drawing::Font(L"Arial", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->KennwortLabel->Location = System::Drawing::Point(27, 81);
+			this->KennwortLabel->Name = L"KennwortLabel";
+			this->KennwortLabel->Size = System::Drawing::Size(76, 16);
+			this->KennwortLabel->TabIndex = 3;
+			this->KennwortLabel->Text = L"Kennwort";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(-1, 249);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(522, 172);
+			this->pictureBox2->TabIndex = 5;
+			this->pictureBox2->TabStop = false;
+			// 
+			// passwortvergessenButton
+			// 
+			this->passwortvergessenButton->Location = System::Drawing::Point(186, 152);
+			this->passwortvergessenButton->Name = L"passwortvergessenButton";
+			this->passwortvergessenButton->Size = System::Drawing::Size(112, 45);
+			this->passwortvergessenButton->TabIndex = 3;
+			this->passwortvergessenButton->Text = L"Passwort vergessen\?";
+			this->passwortvergessenButton->UseVisualStyleBackColor = true;
+			// 
+			// txt_Benutzername
+			// 
+			this->txt_Benutzername->Location = System::Drawing::Point(198, 33);
+			this->txt_Benutzername->Name = L"txt_Benutzername";
+			this->txt_Benutzername->Size = System::Drawing::Size(254, 22);
+			this->txt_Benutzername->TabIndex = 0;
+			// 
+			// txt_Kennwort
+			// 
+			this->txt_Kennwort->Location = System::Drawing::Point(198, 81);
+			this->txt_Kennwort->Name = L"txt_Kennwort";
+			this->txt_Kennwort->PasswordChar = '*';
+			this->txt_Kennwort->Size = System::Drawing::Size(254, 22);
+			this->txt_Kennwort->TabIndex = 1;
+			// 
+			// btn_passwortAendern
+			// 
+			this->btn_passwortAendern->Location = System::Drawing::Point(340, 152);
+			this->btn_passwortAendern->Name = L"btn_passwortAendern";
+			this->btn_passwortAendern->Size = System::Drawing::Size(112, 45);
+			this->btn_passwortAendern->TabIndex = 4;
+			this->btn_passwortAendern->Text = L"Passwort ändern";
+			this->btn_passwortAendern->UseVisualStyleBackColor = true;
+			// 
+			// loginFenster
+			// 
+			this->AllowDrop = true;
+			this->AutoScaleDimensions = System::Drawing::SizeF(7, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(551, 514);
-			this->Name = L"LogInFenster";
-			this->Text = L"LogInFenster";
+			this->AutoSize = true;
+			this->AutoValidate = System::Windows::Forms::AutoValidate::EnablePreventFocusChange;
+			this->BackColor = System::Drawing::SystemColors::Window;
+			this->ClientSize = System::Drawing::Size(517, 421);
+			this->Controls->Add(this->btn_passwortAendern);
+			this->Controls->Add(this->txt_Kennwort);
+			this->Controls->Add(this->txt_Benutzername);
+			this->Controls->Add(this->passwortvergessenButton);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->KennwortLabel);
+			this->Controls->Add(this->BenutzernameLabel);
+			this->Controls->Add(this->logInButton);
+			this->Font = (gcnew System::Drawing::Font(L"Arial", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ForeColor = System::Drawing::SystemColors::MenuText;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"loginFenster";
+			this->Text = L"Imperium Login";
+			this->Load += gcnew System::EventHandler(this, &loginFenster::loginFenster_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+
+
+
+	public:
+		String ^ getBenutzername() {
+			return this->txt_Benutzername->Text;
+		}
+
+		String^ getKennwort() {
+			return this->txt_Kennwort->Text;
+		}
+
+		void clear() {
+			this->txt_Benutzername->Text = "";
+			this->txt_Kennwort->Text = "";
+		}
+
+
+			 //Beim Laden des Fensters wird eine Sound Datei abgespielt
+	private: System::Void loginFenster_Load(System::Object^  sender, System::EventArgs^  e) {
+		sound->SoundLocation = "Sounds/soundImperialMarch.wav";
+		sound->Load();
+		sound->Play();
+
+	}
+
 	};
 }
---
+++ Zeiterfassungssystem/Zeiterfassungssystem/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Von Microsoft Visual C++ generierte Includedatei.
+// Verwendet durch Zeiterfassungssystem.rc
+//
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
+#define IDR_JPG1                        104
+#define IDR_JPG2                        105
+#define IDR_JPG3                        106
+#define IDR_JPG4                        107
+#define IDR_JPG5                        108
+#define IDR_JPG6                        109
+#define IDR_JPG7                        110
+#define IDR_JPG8                        111
+#define IDR_JPG9                        112
+#define IDR_JPG10                       113
+#define IDR_JPG11                       114
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
