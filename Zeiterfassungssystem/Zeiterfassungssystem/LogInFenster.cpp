12ec8f2a83062228a9d34db0b9e60a53c2dda8a0
Jannick Schwartz <jannick.schwartz@gmail.com>
2018-08-27 19:39:46 +0200

    Commit Einstiegspunkt festgelegt

--- Zeiterfassungssystem/Zeiterfassungssystem/LogInFenster.cpp
+++ Zeiterfassungssystem/Zeiterfassungssystem/LogInFenster.cpp
@@ -1,2 +1,12 @@
 #include "LogInFenster.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Zeiterfassungssystem::loginFenster form;
+	Application::Run(%form);
+}
