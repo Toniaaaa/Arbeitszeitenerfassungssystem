f70a18ec2463a96b86769a41321abc89c9d3203a
Jannick Schwartz <jannick.schwartz@gmail.com>
2018-09-07 12:30:08 +0200

    Reguläre Ausdrücke zum checken der Textboxen

--- Zeiterfassungssystem/Zeiterfassungssystem/RegistrierungsFenster.h
+++ Zeiterfassungssystem/Zeiterfassungssystem/RegistrierungsFenster.h
@@ -24,7 +24,10 @@ namespace Zeiterfassungssystem {
 	private:
 		Unternehmen ^ unternehmen;
 		StartseiteVorgesetzte^ startseitevorgesetzte;
-		Vorgesetzter^ vorgesetzter;
+	private: System::Windows::Forms::TextBox^  txt_name;
+			 Vorgesetzter^ vorgesetzter;
+
+
 	public:
 
 		RegistrierungsFenster(void)
@@ -47,7 +50,7 @@ namespace Zeiterfassungssystem {
 			}
 		}
 
-	private: System::Windows::Forms::TextBox^  txt_name;
+
 	private: System::Windows::Forms::TextBox^  txt_vorname;
 	private: System::Windows::Forms::TextBox^  txt_personalnummer;
 	private: System::Windows::Forms::TextBox^  txt_passwort;
@@ -78,7 +81,6 @@ namespace Zeiterfassungssystem {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->txt_name = (gcnew System::Windows::Forms::TextBox());
 			this->txt_vorname = (gcnew System::Windows::Forms::TextBox());
 			this->txt_personalnummer = (gcnew System::Windows::Forms::TextBox());
 			this->txt_passwort = (gcnew System::Windows::Forms::TextBox());
@@ -96,42 +98,67 @@ namespace Zeiterfassungssystem {
 			this->lbl_urlaubstage = (gcnew System::Windows::Forms::Label());
 			this->lbl_rolle = (gcnew System::Windows::Forms::Label());
 			this->lbl_Registrierung = (gcnew System::Windows::Forms::Label());
+			this->txt_name = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
-			this->txt_name->Location = System::Drawing::Point(161, 152);
-			this->txt_name->Name = L"txt_name";
-			this->txt_name->Size = System::Drawing::Size(351, 22);
-			this->txt_name->TabIndex = 2;
+			// 
+			// txt_vorname
+			// 
 			this->txt_vorname->Location = System::Drawing::Point(161, 180);
 			this->txt_vorname->Name = L"txt_vorname";
 			this->txt_vorname->Size = System::Drawing::Size(351, 22);
 			this->txt_vorname->TabIndex = 1;
+			this->txt_vorname->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &RegistrierungsFenster::txt_vorname_Validating);
+			// 
+			// txt_personalnummer
+			// 
 			this->txt_personalnummer->Location = System::Drawing::Point(161, 239);
 			this->txt_personalnummer->Name = L"txt_personalnummer";
 			this->txt_personalnummer->Size = System::Drawing::Size(351, 22);
 			this->txt_personalnummer->TabIndex = 0;
+			this->txt_personalnummer->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &RegistrierungsFenster::txt_personalnummer_Validating);
+			// 
+			// txt_passwort
+			// 
 			this->txt_passwort->Location = System::Drawing::Point(161, 267);
 			this->txt_passwort->Name = L"txt_passwort";
 			this->txt_passwort->Size = System::Drawing::Size(351, 22);
 			this->txt_passwort->TabIndex = 19;
+			// 
+			// txt_abteilung
+			// 
 			this->txt_abteilung->FormattingEnabled = true;
 			this->txt_abteilung->Location = System::Drawing::Point(161, 209);
 			this->txt_abteilung->Name = L"txt_abteilung";
 			this->txt_abteilung->Size = System::Drawing::Size(351, 24);
 			this->txt_abteilung->TabIndex = 25;
+			// 
+			// txt_arbeitsstunden
+			// 
 			this->txt_arbeitsstunden->Location = System::Drawing::Point(161, 297);
 			this->txt_arbeitsstunden->Name = L"txt_arbeitsstunden";
 			this->txt_arbeitsstunden->Size = System::Drawing::Size(351, 22);
 			this->txt_arbeitsstunden->TabIndex = 23;
+			this->txt_arbeitsstunden->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &RegistrierungsFenster::txt_arbeitsstunden_Validating);
+			// 
+			// txt_urlaubstage
+			// 
 			this->txt_urlaubstage->Location = System::Drawing::Point(161, 327);
 			this->txt_urlaubstage->Name = L"txt_urlaubstage";
 			this->txt_urlaubstage->Size = System::Drawing::Size(351, 22);
 			this->txt_urlaubstage->TabIndex = 22;
+			this->txt_urlaubstage->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &RegistrierungsFenster::txt_urlaubstage_Validating);
+			// 
+			// txt_Rolle
+			// 
 			this->txt_Rolle->FormattingEnabled = true;
 			this->txt_Rolle->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Mitarbeiter", L"Vorgesetzter" });
 			this->txt_Rolle->Location = System::Drawing::Point(161, 361);
 			this->txt_Rolle->Name = L"txt_Rolle";
 			this->txt_Rolle->Size = System::Drawing::Size(349, 24);
 			this->txt_Rolle->TabIndex = 33;
+			// 
+			// btn_mitarbeiter_hinzufuegen
+			// 
 			this->btn_mitarbeiter_hinzufuegen->Location = System::Drawing::Point(161, 408);
 			this->btn_mitarbeiter_hinzufuegen->Name = L"btn_mitarbeiter_hinzufuegen";
 			this->btn_mitarbeiter_hinzufuegen->Size = System::Drawing::Size(171, 35);
@@ -139,66 +166,105 @@ namespace Zeiterfassungssystem {
 			this->btn_mitarbeiter_hinzufuegen->Text = L"Hinzufügen";
 			this->btn_mitarbeiter_hinzufuegen->UseVisualStyleBackColor = true;
 			this->btn_mitarbeiter_hinzufuegen->Click += gcnew System::EventHandler(this, &RegistrierungsFenster::btn_mitarbeiter_hinzufuegen_Click);
+			// 
+			// lbl_name
+			// 
 			this->lbl_name->AutoSize = true;
 			this->lbl_name->Location = System::Drawing::Point(16, 157);
 			this->lbl_name->Name = L"lbl_name";
 			this->lbl_name->Size = System::Drawing::Size(45, 17);
 			this->lbl_name->TabIndex = 8;
 			this->lbl_name->Text = L"Name";
+			// 
+			// lbl_vorname
+			// 
 			this->lbl_vorname->AutoSize = true;
 			this->lbl_vorname->Location = System::Drawing::Point(14, 185);
 			this->lbl_vorname->Name = L"lbl_vorname";
 			this->lbl_vorname->Size = System::Drawing::Size(65, 17);
 			this->lbl_vorname->TabIndex = 9;
 			this->lbl_vorname->Text = L"Vorname";
+			// 
+			// lbl_abteilung
+			// 
 			this->lbl_abteilung->AutoSize = true;
 			this->lbl_abteilung->Location = System::Drawing::Point(15, 216);
 			this->lbl_abteilung->Name = L"lbl_abteilung";
 			this->lbl_abteilung->Size = System::Drawing::Size(67, 17);
 			this->lbl_abteilung->TabIndex = 13;
 			this->lbl_abteilung->Text = L"Abteilung";
+			// 
+			// lbl_personalnummer
+			// 
 			this->lbl_personalnummer->AutoSize = true;
 			this->lbl_personalnummer->Location = System::Drawing::Point(15, 244);
 			this->lbl_personalnummer->Name = L"lbl_personalnummer";
 			this->lbl_personalnummer->Size = System::Drawing::Size(115, 17);
 			this->lbl_personalnummer->TabIndex = 14;
 			this->lbl_personalnummer->Text = L"Personalnummer";
+			// 
+			// lbl_passwort
+			// 
 			this->lbl_passwort->AutoSize = true;
 			this->lbl_passwort->Location = System::Drawing::Point(15, 272);
 			this->lbl_passwort->Name = L"lbl_passwort";
 			this->lbl_passwort->Size = System::Drawing::Size(65, 17);
 			this->lbl_passwort->TabIndex = 17;
 			this->lbl_passwort->Text = L"Passwort";
+			// 
+			// lbl_arbeitsstunden
+			// 
 			this->lbl_arbeitsstunden->AutoSize = true;
 			this->lbl_arbeitsstunden->Location = System::Drawing::Point(15, 302);
 			this->lbl_arbeitsstunden->Name = L"lbl_arbeitsstunden";
 			this->lbl_arbeitsstunden->Size = System::Drawing::Size(103, 17);
 			this->lbl_arbeitsstunden->TabIndex = 20;
 			this->lbl_arbeitsstunden->Text = L"Arbeitsstunden";
+			// 
+			// lbl_urlaubstage
+			// 
 			this->lbl_urlaubstage->AutoSize = true;
 			this->lbl_urlaubstage->Location = System::Drawing::Point(16, 330);
 			this->lbl_urlaubstage->Name = L"lbl_urlaubstage";
 			this->lbl_urlaubstage->Size = System::Drawing::Size(85, 17);
 			this->lbl_urlaubstage->TabIndex = 21;
 			this->lbl_urlaubstage->Text = L"Urlaubstage";
+			// 
+			// lbl_rolle
+			// 
 			this->lbl_rolle->AutoSize = true;
 			this->lbl_rolle->Location = System::Drawing::Point(16, 361);
 			this->lbl_rolle->Name = L"lbl_rolle";
 			this->lbl_rolle->Size = System::Drawing::Size(40, 17);
 			this->lbl_rolle->TabIndex = 32;
 			this->lbl_rolle->Text = L"Rolle";
+			// 
+			// lbl_Registrierung
+			// 
 			this->lbl_Registrierung->AutoSize = true;
-			this->lbl_Registrierung->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->lbl_Registrierung->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->lbl_Registrierung->Location = System::Drawing::Point(153, 42);
 			this->lbl_Registrierung->Name = L"lbl_Registrierung";
 			this->lbl_Registrierung->Size = System::Drawing::Size(260, 44);
 			this->lbl_Registrierung->TabIndex = 34;
 			this->lbl_Registrierung->Text = L"Registrierung";
+			// 
+			// txt_name
+			// 
+			this->txt_name->Location = System::Drawing::Point(161, 152);
+			this->txt_name->Name = L"txt_name";
+			this->txt_name->Size = System::Drawing::Size(351, 22);
+			this->txt_name->TabIndex = 35;
+			this->txt_name->Validating += gcnew System::ComponentModel::CancelEventHandler(this, &RegistrierungsFenster::txt_name_Validating);
+			// 
+			// RegistrierungsFenster
+			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Window;
 			this->ClientSize = System::Drawing::Size(524, 456);
+			this->Controls->Add(this->txt_name);
 			this->Controls->Add(this->lbl_Registrierung);
 			this->Controls->Add(this->lbl_rolle);
 			this->Controls->Add(this->lbl_urlaubstage);
@@ -216,7 +282,6 @@ namespace Zeiterfassungssystem {
 			this->Controls->Add(this->txt_passwort);
 			this->Controls->Add(this->txt_personalnummer);
 			this->Controls->Add(this->txt_vorname);
-			this->Controls->Add(this->txt_name);
 			this->Name = L"RegistrierungsFenster";
 			this->Text = L"Registrierung";
 			this->Load += gcnew System::EventHandler(this, &RegistrierungsFenster::registrierungsFenster_Load);
@@ -327,18 +392,11 @@ namespace Zeiterfassungssystem {
 			this->DialogResult = System::Windows::Forms::DialogResult::None;
 			fehler = true;
 		}
-		
 		//ÄNDERUNG
 		if (fehler) {
 			System::Windows::Forms::MessageBox::Show("Bitte füllen Sie alle Felder aus!", "Fehler!",
 				MessageBoxButtons::OK, MessageBoxIcon::Error);
-		}
-		else if (!Int32::TryParse(txt_arbeitsstunden->Text, parse)) {
-			MessageBox::Show("Arbeitsstunden: Bitte geben Sie eine ganze Zahl ein!");
-		}
-		else if (!Int32::TryParse(txt_urlaubstage->Text, parse)) {
-			MessageBox::Show("Urlaubstage: Bitte geben Sie eine ganze Zahl ein!");
-		}
+		} 
 		else {
 			if (this->txt_Rolle->SelectedItem->ToString()->Equals("Mitarbeiter")) {
 				Abteilung^ abteilung = nullptr;
@@ -367,5 +425,60 @@ namespace Zeiterfassungssystem {
 			}
 		}
 	}
+private: System::Void txt_name_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+
+	String^ pattern = "[A-Z][a-z]+";
+	Regex^ regex = gcnew Regex(pattern);
+
+	if (!regex->IsMatch(txt_name->Text)) {
+		MessageBox::Show("Bitte geben Sie nur Buchstaben ein.", "Fehler!", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		txt_name->Text = "";
+	}
+
+}
+private: System::Void txt_vorname_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+
+	String^ pattern = "[A-Z][a-z]+";
+	Regex^ regex = gcnew Regex(txt_vorname->Text);
+
+	if (!regex->IsMatch(pattern)) {
+		MessageBox::Show("Bitte geben Sie nur Buchstaben ein.", "Fehler!", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		txt_vorname->Text = "";
+	}
+
+}
+private: System::Void txt_personalnummer_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+
+	String^ pattern = "[0-9]+";
+	Regex^ regex = gcnew Regex(pattern);
+
+	if (!regex->IsMatch(txt_personalnummer->Text)) {
+		MessageBox::Show("Bitte geben Sie als Personalnummer eine ganze Zahl ein!", "Fehler!", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		txt_personalnummer->Text = "";
+	}
+
+}
+private: System::Void txt_arbeitsstunden_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+
+	String^ pattern = "[0-9]+";
+	Regex^ regex = gcnew Regex(pattern);
+
+	if (!regex->IsMatch(txt_arbeitsstunden->Text)) {
+		MessageBox::Show("Bitte geben Sie als Arbeitsstunden eine ganze Zahl ein!", "Fehler!", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		txt_arbeitsstunden->Text = "";
+	}
+
+}
+private: System::Void txt_urlaubstage_Validating(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+
+	String^ pattern = "[0-9]+";
+	Regex^ regex = gcnew Regex(pattern);
+
+	if (!regex->IsMatch(txt_urlaubstage->Text)) {
+		MessageBox::Show("Bitte geben Sie als Urlaubstage eine ganze Zahl ein!", "Fehler!", MessageBoxButtons::OK, MessageBoxIcon::Error);
+		txt_urlaubstage->Text = "";
+	}
+
+}
 };
 }
--- Zeiterfassungssystem/Zeiterfassungssystem/StatistikFenster.h
+++ Zeiterfassungssystem/Zeiterfassungssystem/StatistikFenster.h
@@ -17,7 +17,10 @@ namespace Zeiterfassungssystem {
 	{
 	private: 
 		AenderungsantragsFenster ^ aenderungsantrag;
-		Angestellter^ angestellterAkt;
+	private: System::Windows::Forms::Label^  label;
+	private: System::Windows::Forms::Label^  lbl_gesamtstunden;
+
+			 Angestellter^ angestellterAkt;
 	public:
 		StatistikFenster(void)
 		{
@@ -56,34 +59,36 @@ namespace Zeiterfassungssystem {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->btn_aenderungsanfrage = (gcnew System::Windows::Forms::Button());
+			this->label = (gcnew System::Windows::Forms::Label());
+			this->lbl_gesamtstunden = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// chart1
 			// 
-			chartArea1->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea1);
-			legend1->Name = L"Legend1";
-			this->chart1->Legends->Add(legend1);
+			chartArea2->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea2);
+			legend2->Name = L"Legend1";
+			this->chart1->Legends->Add(legend2);
 			this->chart1->Location = System::Drawing::Point(46, 37);
 			this->chart1->Name = L"chart1";
-			series1->ChartArea = L"ChartArea1";
-			series1->IsValueShownAsLabel = true;
-			series1->Legend = L"Legend1";
-			series1->Name = L"Arbeitsstunden";
-			this->chart1->Series->Add(series1);
-			this->chart1->Size = System::Drawing::Size(568, 316);
+			series2->ChartArea = L"ChartArea1";
+			series2->IsValueShownAsLabel = true;
+			series2->Legend = L"Legend1";
+			series2->Name = L"Arbeitsstunden";
+			this->chart1->Series->Add(series2);
+			this->chart1->Size = System::Drawing::Size(1340, 316);
 			this->chart1->TabIndex = 0;
 			this->chart1->Text = L"chart1";
 			// 
 			// btn_aenderungsanfrage
 			// 
-			this->btn_aenderungsanfrage->Location = System::Drawing::Point(353, 389);
+			this->btn_aenderungsanfrage->Location = System::Drawing::Point(46, 390);
 			this->btn_aenderungsanfrage->Name = L"btn_aenderungsanfrage";
 			this->btn_aenderungsanfrage->Size = System::Drawing::Size(250, 58);
 			this->btn_aenderungsanfrage->TabIndex = 1;
@@ -91,27 +96,51 @@ namespace Zeiterfassungssystem {
 			this->btn_aenderungsanfrage->UseVisualStyleBackColor = true;
 			this->btn_aenderungsanfrage->Click += gcnew System::EventHandler(this, &StatistikFenster::btn_aenderungsanfrage_Click);
 			// 
+			// label
+			// 
+			this->label->AutoSize = true;
+			this->label->Location = System::Drawing::Point(352, 411);
+			this->label->Name = L"label";
+			this->label->Size = System::Drawing::Size(250, 17);
+			this->label->TabIndex = 2;
+			this->label->Text = L"Arbeitsstunden diesen Monat gesamt: ";
+			// 
+			// lbl_gesamtstunden
+			// 
+			this->lbl_gesamtstunden->AutoSize = true;
+			this->lbl_gesamtstunden->Location = System::Drawing::Point(617, 411);
+			this->lbl_gesamtstunden->Name = L"lbl_gesamtstunden";
+			this->lbl_gesamtstunden->Size = System::Drawing::Size(46, 17);
+			this->lbl_gesamtstunden->TabIndex = 3;
+			this->lbl_gesamtstunden->Text = L"label1";
+			// 
 			// StatistikFenster
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(642, 523);
+			this->ClientSize = System::Drawing::Size(1412, 523);
+			this->Controls->Add(this->lbl_gesamtstunden);
+			this->Controls->Add(this->label);
 			this->Controls->Add(this->btn_aenderungsanfrage);
 			this->Controls->Add(this->chart1);
 			this->Name = L"StatistikFenster";
 			this->Text = L"StatistikFenster";
+			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &StatistikFenster::StatistikFenster_FormClosing);
 			this->Load += gcnew System::EventHandler(this, &StatistikFenster::StatistikFenster_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void StatistikFenster_Load(System::Object^  sender, System::EventArgs^  e) {
 		this->chart1->Series["Arbeitsstunden"]->Points->AddXY("Tag", 8);
-		for (int i = 0; i < angestellterAkt->getAnzahlArbeitstage(); i++) {
-			this->chart1->Series["Arbeitsstunden"]->Points->AddXY("Tag" + i, i);
+		int gesamtstunden = 0;
+		for (int i = 0; i < 50; i++) {
+			this->chart1->Series["Arbeitsstunden"]->Points->AddXY("Tag " + (i+1), i+1);
+			gesamtstunden += i + 1;
 		}
-
+		lbl_gesamtstunden->Text = gesamtstunden + "";
 	}
 	private: System::Void btn_aenderungsanfrage_Click(System::Object^  sender, System::EventArgs^  e) {
 		aenderungsantrag->ShowDialog(this);
@@ -119,5 +148,8 @@ namespace Zeiterfassungssystem {
 	public: void setAngestellterAkt(Angestellter^ angestellter) {
 		this->angestellterAkt = angestellter;
 	}
-	};
+	private: System::Void StatistikFenster_FormClosing(System::Object^  sender, System::Windows::Forms::FormClosingEventArgs^  e) {
+		this->chart1->Series["Arbeitsstunden"]->Points->Clear();
+	}
+};
 }
